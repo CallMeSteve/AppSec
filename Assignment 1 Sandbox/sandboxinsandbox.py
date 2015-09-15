8dfedaa5e6c841094c54f86a46e6702c62b5ad17
Abdullah Sarwar <asarwar@nyu.edu>
2015-09-15 16:39:31 -0400

    adding all the files

---
+++ Assignment 1 Sandbox/sandboxinsandbox.py
@@ -0,0 +1,23 @@
+"""
+Abdullah Sarwar
+CS-UY 4753
+Professor Justin Cappos
+
+"Sanbox in a sandbox" to pass to sandbox.py
+"""
+
+NOT_SAFE = ['import','os','subprocess','sys','exec','eval','_','.','subclasses','bases','NOT_SAFE']
+
+while True:
+	print ">> ",
+	inp = raw_input()
+	
+	for untrusted in NOT_SAFE:
+		if untrusted in inp:
+			print "Cannot use '" + untrusted +"'" 
+
+	try:
+		exec inp
+	
+	except ImportError, NameError, IOError:
+		print "ONLY USE SAFE FUNCTIONS"
